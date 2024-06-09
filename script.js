e802f20d09e7a6d027236ab39846ea621490774d
faima-ahmed <faimaahmeddiya@gmail.com>
2024-06-09 08:10:22 +0600

    Commit 1

--- script.js
+++ script.js
@@ -34,3 +34,11 @@ document.addEventListener('keydown', function (e) {
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
+
+document.querySelector('.header');
+const allSections= document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('.section--1');
+const allButtons= document.getElementsByTagName('button');
+console.log(allButtons);
